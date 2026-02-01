0bc1a7192e95e4fc0c27bd5998c483b962ff3332
mohabasumeih <mohabasumeih@gmail.com>
2026-02-02 05:18:07 +0530

    Fix Elastic Beanstalk config and update requirements

--- application.py
+++ application.py
@@ -39,6 +39,10 @@ def predict_datapoint():
 
         return render_template('home.html', results=results[0])
 
-if __name__ == "__main__":
-    print("Starting Flask app...")
-    app.run(host="127.0.0.1", port=5001)
+#if __name__ == "__main__":
+    #print("Starting Flask app...")
+  #  app.run(host="127.0.0.1", port=5001)
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000)
+
